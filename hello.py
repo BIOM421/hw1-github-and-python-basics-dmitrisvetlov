553eccb0839632cf8a167fbfb7a502ecebed2a53
Dmitri Svetlov <123213937+dmitrisvetlov@users.noreply.github.com>
2024-05-20 20:25:56 -0600

    Complete Hello, World! script

--- hello.py
+++ hello.py
@@ -1,2 +1,12 @@
+greeting = "Hello World!"
+
 def hello_world():
-	return "Hello"
+	return greeting
+
+def hello_world_n(N):
+    output = ''
+    for cntr in range(N):
+        if cntr > 0:
+            output = output + ' ' # Add leading separating space
+        output = output + greeting
+    return output
